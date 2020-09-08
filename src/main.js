cc023beb970a3c3cfcd7eaada63c1cdb21a4df54
eugene.r <eugene.ryabczinski@yandex.ru>
2020-09-08 20:03:26 +0300

    start api

--- src/main.js
+++ src/main.js
@@ -10,8 +10,34 @@ import MenuPresenter from './presenter/menu';
 import EventsModel from './models/event';
 import FiltersModel from './models/filters';
 
+
+
+import Api from "./api.js";
+
+const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
+const END_POINT = `https://12.ecmascript.pages.academy/big-trip`;
+
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getPoints().then((point) => {
+  console.log(point);
+  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+  // а ещё на сервере используется snake_case, а у нас camelCase.
+  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+  // Есть вариант получше - паттерн "Адаптер"
+});
+
+
+
+
+
+
+
+
 const EVENT_COUNT = 10;
 const events = new Array(EVENT_COUNT).fill().map(generateEvent);
+
+console.log(events);
 
 const eventsModel = new EventsModel();
 eventsModel.setEvents(events);
